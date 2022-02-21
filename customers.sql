594cce1eeba0643481067835df32250b5340bf62
Ubuntu <ubuntu@ip-172-31-21-57.us-east-2.compute.internal>
2022-02-21 06:21:39 +0000

    Updated query to include CTE

--- customers.sql
+++ customers.sql
@@ -1,11 +1,18 @@
+with customer_order as (
+    select
+    customer_id
+    , count(*) as order_count
+    , min(created_at) as first_order_date
+from `analytics-engineers-club.coffee_shop.orders`
+group by customer_id
+)
 select 
-    c.id as id,
-    c.name as name,
-    c.email as email,
-    o.created_at as first_order_at,
-    count(c.id) over (partition by c.id) as number_of_orders
-from `analytics-engineers-club.coffee_shop.customers` as c
-inner join `analytics-engineers-club.coffee_shop.orders` as o
-on o.customer_id = c.id
-group by c.id, c.name, c.email, o.created_at
-order by first_order_at limit 5;
+      c.id as customer_id
+    , c.name as name
+    , c.email as email
+    , co.first_order_date as first_order_date
+    , co.order_count as total_orders
+    from `analytics-engineers-club.coffee_shop.customers` as c
+join customer_order as co
+on co.customer_id = c.id
+limit 5;
